4049f148940ae02dd9d91ced34b98b310bf0b6b5
Taylor Williams <taylor@thewilliams.net>
2018-05-12 23:04:41 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,9 +2,8 @@ def my_collect(array)
   counter = 0
   new_array = []
   while counter < array.length
-    new_array << yield array[counter]
+    new_array << yield(array[counter])
     counter += 1
   end
-  return new_array
+  new_array
 end
-
